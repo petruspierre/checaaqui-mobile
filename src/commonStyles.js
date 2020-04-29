c5981ba6019f7acfa725cc4775f1f22166dee7f7
Petrus Pierre <petrus.pierre@academico.ifpb.edu.br>
2020-04-29 18:05:43 -0300

    created some pages

---
+++ src/commonStyles.js
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    primary: '#327DCB',
+    secondary: '#5494D6',
+    dark: '#094B91',
+    semidark: '#0F66C1',
+    light: '#7EB0E4',
+  },
+  metrics: {
+    padding: 15,
+    margin: 15
+  },
+  fontFamily: 'Tauri'
+}
---
+++ src/pages/Ranking/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View} from 'react-native'
+
+import styles from './styles.js'
+
+export default function Profile(){
+  return (
+    <View />
+  )
+}
